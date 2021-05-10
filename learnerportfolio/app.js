69b739444fa2f88b972083d97a24c7551cd3a611
J.R. Smith <75698426+itsjrsmith@users.noreply.github.com>
2021-05-10 14:49:23 -0700

    deleted express imports
    got rid off some of the backend stuff i put in for no reason.

--- learnerportfolio/app.js
+++ learnerportfolio/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
@@ -251,29 +251,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   
   })
-=======
-const express = require('express');
-const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
-const path = require('path');
-const nodemailer = require('nodemailer');
 
-const app = express();
-
-// View engine setup
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-// Static folder
-app.use('public', express.static(path.join(__dirname, 'public')));
-
-// Body parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    res.send('hello');
-});
-
-app.listen(3000, () => console.log('Server started...'));
->>>>>>> master
